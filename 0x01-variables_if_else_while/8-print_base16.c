bc6cc56e819e8d176409a26ed6e2b58fdd56d625
bchen528 <bchen720@gmail.com>
2018-01-18 23:16:29 +0000

    print all numbers of base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0, exit program
+ */
+
+int main(void)
+{
+char c = 0;
+
+while (c < 10)
+{
+putchar('0' + c);
+c++;
+}
+
+c = 'a';
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
