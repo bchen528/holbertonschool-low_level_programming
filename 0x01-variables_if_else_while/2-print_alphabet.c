15a85635fb47ee7c3ddd4f7234027f53e4cec689
bchen528 <bchen720@gmail.com>
2018-01-18 19:58:48 +0000

    putchar to print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0, exit program
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
