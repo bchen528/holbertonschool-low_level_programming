bc61ba604d0837cb9056886bcdc9c23d556c9d31
bchen528 <bchen720@gmail.com>
2018-01-18 22:09:09 +0000

    prints all single digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0, exit program
+ */
+
+int main(void)
+{
+int  digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+printf("%d", digit);
+}
+putchar('\n');
+return (0);
+}
