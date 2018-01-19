18df5b1206209c48060b9bed7e1716318136b1de
bchen528 <bchen720@gmail.com>
2018-01-19 05:12:50 +0000

    betty check

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,17 @@ int main(void)
 {
 	int a = 0;
 	int b = a + 1;
-	
+
 	while (a <= 9)
 	{
 		while (b < 10)
 		{
 			putchar('0' + a);
 			putchar('0' + b);
-			
 			if (!(a == 8 && b == 9))
 			{
-		putchar(',');
-		putchar(' ');
+				putchar(',');
+				putchar(' ');
 			}
 			b++;
 		}
