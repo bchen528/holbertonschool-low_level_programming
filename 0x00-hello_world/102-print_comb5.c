c44c1b1d408b5e5040038af612d1852100228c85
bchen528 <bchen720@gmail.com>
2018-01-19 21:21:42 +0000

    print all possible combos of 2 digit numbers

---
+++ 0x00-hello_world/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0, exit program
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = a + 1; b < 100; b++)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (!(a == 98 && b == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
