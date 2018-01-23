596715f3fb36a2edfa1b8a643b8edecbcf633846
bchen528 <bchen720@gmail.com>
2018-01-23 05:32:38 +0000

    prints minutes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints that last digit of a number
+ * @n: type to be checked
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -1 * (n % 10);
+	}
+	else 
+	{
+		n = n % 10;
+	}
+	_putchar(n + '0');
+	return (n);
+}
