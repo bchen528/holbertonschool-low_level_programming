59a06f6e318bd9842605c51c052f711fd0055e8a
bchen528 <bchen720@gmail.com>
2018-01-23 01:53:42 +0000

    first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,24 @@
 #include "holberton.h"
 
 /**
- * main - print alphabet 10x
+ * print_alphabet_x10 - print alphabet 10x
  * Return: 0, exit program
  */
 
 void print_alphabet_x10(void)
 {
 	char letter = 'a';
+	int i = 0;
 
+	while (i < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+		letter = 'a';
+	}
 }
