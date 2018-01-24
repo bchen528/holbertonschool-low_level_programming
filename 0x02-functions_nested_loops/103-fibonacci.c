7df1423db041442f68c04515ea2c5582f567c58f
bchen528 <bchen720@gmail.com>
2018-01-24 23:24:15 +0000

    print even Fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**                                                                                                                                         
+ * main - prints first 50 Fibonacci numbers starting with 1 and 2                                                                           
+ * Return: 0                                                                                                                                
+ */
+
+int main(void)
+{
+        long previousNum = 1;
+        long currentNum = 2;
+        long nextNum = 0;
+        long sum = 2;
+
+        while (nextNum <= 4000000)
+        {
+                nextNum = previousNum + currentNum;
+		if ((nextNum % 2) == 0)
+		{
+			sum = sum + nextNum;
+		}
+                previousNum = currentNum;
+                currentNum = nextNum;
+                
+        }
+        printf("%ld\n", sum);
+        return (0);
+}
