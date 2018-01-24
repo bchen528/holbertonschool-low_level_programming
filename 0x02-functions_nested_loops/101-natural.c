8a23edb2f1330c246ab9b8bff4f213a769bad313
bchen528 <bchen720@gmail.com>
2018-01-24 22:07:05 +0000

    prints multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_sum - computes and prints sum of all multiples of 3 or 5 below 1024
+ * main - computes and prints sum of all multiples of 3 or 5 below 1024
  * Return: 0
  */
 
