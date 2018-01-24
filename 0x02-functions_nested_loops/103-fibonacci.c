165589144b68a982dcc7c858fb2e560bec503abb
bchen528 <bchen720@gmail.com>
2018-01-24 23:32:25 +0000

    betty check

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,28 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/**                                                                                                                                         
- * main - prints first 50 Fibonacci numbers starting with 1 and 2                                                                           
- * Return: 0                                                                                                                                
+/**
+ * main - prints even Fibonacci numbers up to 4000000, starting with 1 and 2
+ * Return: 0
  */
 
 int main(void)
 {
-        long previousNum = 1;
-        long currentNum = 2;
-        long nextNum = 0;
-        long sum = 2;
+	long previousNum = 1;
+	long currentNum = 2;
+	long nextNum = 0;
+	long sum = 2;
 
-        while (nextNum <= 4000000)
-        {
-                nextNum = previousNum + currentNum;
+	while (nextNum <= 4000000)
+	{
+		nextNum = previousNum + currentNum;
 		if ((nextNum % 2) == 0)
 		{
 			sum = sum + nextNum;
 		}
-                previousNum = currentNum;
-                currentNum = nextNum;
-                
-        }
-        printf("%ld\n", sum);
-        return (0);
+		previousNum = currentNum;
+		currentNum = nextNum;
+	}
+	printf("%ld\n", sum);
+	return (0);
 }
