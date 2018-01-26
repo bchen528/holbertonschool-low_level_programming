5f5c8e4c0e99098a8c0e5968ba1a4c5de97a4906
bchen528 <bchen720@gmail.com>
2018-01-26 00:33:21 +0000

    prints square

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -2,10 +2,23 @@
 
 /**
  * print_square - prints a square
+ * @size: parameter to be checked
  * Return: 0
  */
 
 void print_square(int size)
 {
+	int w, h;
+
+	for (h = 0; h < size; h++)
+	{
+		for (w = 0; w < size; w++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
 
 }
