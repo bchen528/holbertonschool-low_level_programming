9db3688865c5bc19a213ffd7a66083f0c50377c1
bchen528 <bchen720@gmail.com>
2018-01-26 04:06:10 +0000

    algorithm fix

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -9,19 +9,22 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
-	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-		}
-	}
-	if (n != 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			if (n == i)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar('_');
+			}
+		}
 	}
 }
