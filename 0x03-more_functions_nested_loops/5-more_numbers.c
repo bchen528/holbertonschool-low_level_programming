e60d651817877c04d2f82c567c88129e01bb8915
bchen528 <bchen720@gmail.com>
2018-01-26 05:43:10 +0000

    first commit

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,9 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i <= 9)
-			{
-				_putchar(i + '0');
-			}
-			else
-			{
+			if (i > 9)
 				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
