e090ee01f8528bcba54f785ea668ba641e4769c2
bchen528 <bchen720@gmail.com>
2018-01-26 03:36:25 +0000

    betty check

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -3,24 +3,28 @@
 /**
  * print_line - draws a straight line in terminal
  * @n: parameter to be checked
- * 
  */
 
 void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n != 0)
 	{
-		if (n <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
 		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
+			if (n <= 0)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar('_');
+			}
 		}
 	}
-	if (n != 0)
-		_putchar('\n');
 }
