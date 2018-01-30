4fcc1fbde065a5549bb149a882f7a673352067b5
bchen528 <bchen720@gmail.com>
2018-01-30 17:46:14 +0000

    move array declaration to top

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,12 @@ void rev_string(char *s)
 	int index;
 	int length = 0;
 	int i = 0;
+	char rev[9];
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-
-	char rev[length];
 
 	length--;
 	index = length;
@@ -25,6 +24,7 @@ void rev_string(char *s)
 		rev[length - index] = s[index];
 		index--;
 	}
+
 	++length;
 	while (i < length)
 	{
