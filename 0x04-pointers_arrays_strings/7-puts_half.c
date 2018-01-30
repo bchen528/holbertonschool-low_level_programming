3809b21968d900e8f363739f973de92bd1e2a61b
bchen528 <bchen720@gmail.com>
2018-01-30 05:25:33 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: parameter to be checked
+ */ 
+
+
+void puts_half(char *str)
+{
+	int index;
+	int length;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	index = length / 2;
+	while (index <= 10)
+	{
+		_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
+}
