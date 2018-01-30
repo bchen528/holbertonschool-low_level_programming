ff5275088bfcc35d7dd44fba9371a31c73bf1655
bchen528 <bchen720@gmail.com>
2018-01-30 05:09:01 +0000

    first commit

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -2,5 +2,20 @@
 
 /**
  * puts2 - prints one char out of 2 of a string
- * 
+ * @str: parameter to be checked
  */
+
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (index < 10)
+	{	
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+		index++;
+	}
+	_putchar('\n');
+}
