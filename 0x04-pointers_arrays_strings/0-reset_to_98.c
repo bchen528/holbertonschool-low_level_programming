c77040a43cce54e4a13e50a8ddbc5ec8d3336469
bchen528 <bchen720@gmail.com>
2018-01-30 00:25:53 +0000

    updates value to 98 with pointer

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes pointer to int as parameter and change value to 98
+ * @n: parameter to be checked
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
