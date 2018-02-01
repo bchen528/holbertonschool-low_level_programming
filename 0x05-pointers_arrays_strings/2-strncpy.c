bf07f55b6b0420aea239f41a9b59062965ababf6
bchen528 <bchen720@gmail.com>
2018-02-01 18:39:43 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
