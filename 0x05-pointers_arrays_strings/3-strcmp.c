b5fe01e5bc7c88c98bae64ce2b6c74374f26e410
bchen528 <bchen720@gmail.com>
2018-02-02 07:52:18 +0000

    remove fixed value

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,8 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (s1[0] - s2[0]);
+		return (s1[i] - s2[i]);
 		i++;
 	}
-	return (s1[0] - s2[0]);
+	return (s1[i] - s2[i]);
 }
