2160067cfe97f944d8e7ddec6bb089673a2f4ee3
bchen528 <bchen720@gmail.com>
2018-02-02 07:46:39 +0000

    condense program

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -13,18 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] < s2[i])
-		{
-			return (s1[0] - s2[0]);
-		}
-		else if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (s1[0] - s2[0]);
-		}
+		return (s1[0] - s2[0]);
 		i++;
 	}
 	return (s1[0] - s2[0]);
