c041dc0c4a10beca3942a5c5543f1dc97e275e8a
bchen528 <bchen720@gmail.com>
2018-02-02 23:21:31 +0000

    more elegant code

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -13,12 +13,6 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s1[i] == s2[i])
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else
-			return (0);
 		i++;
 	}
 	return (s1[i] - s2[i]);
