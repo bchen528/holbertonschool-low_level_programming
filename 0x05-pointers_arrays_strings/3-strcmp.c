18df02c9188d243e22d57240890a26d488544525
bchen528 <bchen720@gmail.com>
2018-02-02 08:08:44 +0000

    change algorithm

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,10 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s1[i] == s2[i])
 	{
-		return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		else
+			return (0);
 		i++;
 	}
 	return (s1[i] - s2[i]);
