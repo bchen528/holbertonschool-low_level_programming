cb6c15337c74b3fb64458c2e9d0ad865306148e1
bchen528 <bchen720@gmail.com>
2018-02-02 07:05:28 +0000

    betty check

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -12,18 +12,17 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 97 && s[i] <= 122) /*if 1st letter of word is lowercase, capitalize it*/
-		{
+		if (s[i] >= 97 && s[i] <= 122)
 			s[i] = s[i] - 32;
-		}
 		i++;
 		if (s[i] != '\0')
 		{
 			while (s[i] >= 97 && s[i] <= 122)
-			{
 				i++;
-			}
-			while (is_separator(s[i]) == 1)
+			while (s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!'
+			       || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')'
+			       || s[i] == '{' || s[i] == '}' || s[i] == '\n'
+			       || s[i] == '\t' || s[i] == ' ')
 			{
 				i++;
 			}
@@ -32,40 +31,6 @@ char *cap_string(char *s)
 		{
 			return (s);
 		}
-		
 	}
 	return (s);
-}
-
-/**
- * is_separator - returns 
- * @s: parameter to be checked
- * Return: 0 if not sepecial character, 1 if special character
- */
-
-int is_separator(char *s)
-{
-	int i, j = 0;
-	if (s[i] == ',' 
-	    || s[i] == ';' 
-	    || s[i] == '.' 
-	    || s[i] == '!'
-	    || s[i] == '?' 
-	    || s[i] == '"'
-	    || s[i] == '(' 
-	    || s[i] == ')'
-	    || s[i] == '{' 
-	    || s[i] == '}'
-	    || s[i] == '\n'
-	    || s[i] == '\t'
-	    || s[i] == ' ')
-	{
-		j = 1;
-		return (j);
-	}
-	else
-	{
-		j = 0;
-		return (j);	
-	}
 }
