1773c5125c97cd9493fbdcaaab85266f41b5f38c
bchen528 <bchen720@gmail.com>
2018-02-05 18:45:57 +0000

    first commit

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -50,3 +50,4 @@ int is_separator(char *);
 char *rot13(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
