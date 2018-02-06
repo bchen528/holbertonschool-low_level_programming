a8b4440100e0001de9125953da526aba3a92eb02
bchen528 <bchen720@gmail.com>
2018-02-06 02:08:05 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints sum of the two diagonals of
+ * a square matrix of integers
+ * @a: square matrix
+ * @size: dimension of matrix a
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
