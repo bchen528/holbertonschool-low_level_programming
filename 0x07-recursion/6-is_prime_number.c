fdd116479dd4fc4a86b0fa69f94284b9c4a5f1dc
bchen528 <bchen720@gmail.com>
2018-02-08 21:57:39 +0000

    rearrange helper function

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,22 +1,6 @@
 #include "holberton.h"
 
 /**
- * is_prime_number - find a prime number
- * @n: number to be assessed
- * Return: 1 if n is prime, 0 otherwise
- */
-
-int is_prime_number(int n)
-{
-	int x;
-
-	if (n <= 1)
-		return (0);
-	x = n - 1; /* search for factors of n, don't include n*/
-	return (check_prime(n, x));
-}
-
-/**
  * check_prime - checks if number is prime or not
  * @n: number to be assessed
  * @x: potential factor
@@ -30,4 +14,20 @@ int check_prime(int n, int x)
 	if (n % x == 0)
 		return (0);
 	return (check_prime(n, x - 1));
+}
+
+/**
+ * is_prime_number - find a prime number
+ * @n: number to be assessed
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int x;
+
+	if (n <= 1)
+		return (0);
+	x = n - 1; /* search for factors of n, don't include n*/
+	return (check_prime(n, x));
 }
