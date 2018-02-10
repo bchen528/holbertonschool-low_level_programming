fe197372f105fa86e7452ddb05f60cc05a088030
bchen528 <bchen720@gmail.com>
2018-02-10 01:49:32 +0000

    first commit

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: length of argv, number of command line arguments
+ * @argv: array of of length argc containing command line arguments
+ * Return: program name string
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
