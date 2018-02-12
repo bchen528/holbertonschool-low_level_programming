c16e6ced70613c866866d933168662a41c4d9e41
bchen528 <bchen720@gmail.com>
2018-02-12 07:46:00 +0000

    rearrange empty argument condition

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -14,22 +14,22 @@ int main(int argc, char *argv[])
 	int i;
 	int pos_sum = 0;
 
-	for (i = 1; i < argc; i++)
+	if (*argv[0] == '\0')
 	{
-		if (*argv[0] == '\0')
-		{
-			printf("%d\n", 0);
-			return (0);
-		}
-		else if (!(*argv[i] >= 48 && *argv[i] <= 57) && *argv[0] != '\0')
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	i = 1;
+	while (i < argc)
+	{
+		if (!(*argv[i] >= 48 && *argv[i] <= 57))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		else
-		{
-			pos_sum += atoi(argv[i]);
-		}
+		pos_sum += atoi(argv[i]);
+		i++;
 	}
 	printf("%d\n", pos_sum);
 	return (0);
