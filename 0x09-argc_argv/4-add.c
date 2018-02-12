dfcaed3d65336e506b1fb2ae2c7b0f671bef3c05
bchen528 <bchen720@gmail.com>
2018-02-12 23:04:40 +0000

    add helper fx to look for number/letter string

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -3,6 +3,25 @@
 #include <stdlib.h>
 
 /**
+ * check_args - check if string for numbers and non numbers
+ * @argv: the string to be analyzed
+ * Return: 1 if number, 0 if non-number
+ */
+
+int check_args(argv[i])
+{
+	int i = 0;
+
+	while (argv[i] != '\0')
+	{
+		if (!(argv[i] >= 48 && argv[i] <= 57))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
  * main - adds positive numbers
  * @argc: length of argv array
  * @argv: array containing command line arguments
@@ -11,27 +30,19 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i = 1;
 	int pos_sum = 0;
 
-	if (argc == 1)
+	while (i < argc)
 	{
-		printf("%d\n", 0);
-	}
-	else
-	{
-		i = 1;
-		while (i < argc)
+		if (check_args(argv[i]) == 0)
 		{
-			if (!(*argv[i] >= 48 && *argv[i] <= 57))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			pos_sum += atoi(argv[i]);
-			i++;
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", pos_sum);
+		pos_sum += atoi(argv[i]);
+		i++;
 	}
+	printf("%d\n", pos_sum);
 	return (0);
 }
