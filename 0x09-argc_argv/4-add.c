cdf3525bbecd7e31733506340b18a813e3c949ef
bchen528 <bchen720@gmail.com>
2018-02-12 07:30:03 +0000

    include edge case

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!(*argv[i] >= 48 && *argv[i] <= 57))
+		if (!(*argv[i] >= 48 && *argv[i] <= 57) && *argv[0] != '\0')
 		{
 			printf("Error\n");
 			return (1);
