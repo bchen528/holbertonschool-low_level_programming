91a29398703c85d82960d010a7106fc5771d2ab8
bchen528 <bchen720@gmail.com>
2018-02-13 06:08:54 +0000

    change if else statement

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the minimum number of coins to
@@ -13,28 +14,25 @@ int main(int argc, char *argv[])
 {
 	int coins, change;
 
-	if (argc < 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
+	change = atoi(argv[1]);
+	for (coins = 0; change > 0; coins++)
 	{
-		change = atoi(argv[i]);
-		for (coin = 0; change > 0; coin++)
-		{
-			if (change >= 25)
-				change = change - 25;
-			if (change >= 10)
-				change = change - 10;
-			if (change >= 5)
-				change = change - 5;
-			if (change >= 2)
-				change = change - 2;
-			if (change >= 1)
-				change = change - 1;
-		}
-		printf("%d\n", coins);
+		if (change >= 25)
+			change = change - 25;
+		else if (change >= 10)
+			change = change - 10;
+		else if (change >= 5)
+			change = change - 5;
+		else if (change >= 2)
+			change = change - 2;
+		else if (change >= 1)
+			change = change - 1;
 	}
+	printf("%d\n", coins);
 	return (0);
 }
