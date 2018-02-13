c441bc6650454a24951d7a8c7b41e0d5579d0d35
bchen528 <bchen720@gmail.com>
2018-02-13 21:24:33 +0000

    add all headers

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -69,3 +69,8 @@ int wildcmp(char *s1, char *s2);
 int find_sqrt(int n, int x);
 int check_prime(int n, int x);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
