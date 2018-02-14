227a63b0a3cb9178cfca8e71a9254ea6c1872279
bchen528 <bchen720@gmail.com>
2018-02-14 22:05:26 +0000

    write to malloc statements

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -17,12 +17,24 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	*grid = malloc(sizeof(int *) * (width * height));
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = i - 1; j < width; j--)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
-			*(*(grid + i) + j) = 0;
+			grid[i][j] = 0;
 		}
 	}
 	return (grid);
