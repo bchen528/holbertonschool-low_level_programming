d09a97f86ee75ce26323901c67d8e8f8497d1e85
bchen528 <bchen720@gmail.com>
2018-02-15 23:30:38 +0000

    clean up typecast

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -16,15 +16,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	new = (char *)malloc(nmemb * size);
+	new = malloc(nmemb * size);
 	if (new == NULL)
 		return (NULL);
-
-	for (i = 0; i < nmemb ; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
-		*((char *)new + i) = '0';
+		*((char)new + i) = 0;
 	}
-	*((char *)new + i) = '\0';
 	return (new);
 }
