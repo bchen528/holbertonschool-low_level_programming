5326e60c7163b5dec0f9ba79648783752f102f30
bchen528 <bchen720@gmail.com>
2018-02-19 21:14:09 +0000

    first commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MACRO
+#define MACRO
+/**
+ * struct dog - contains variables for name, age, owner of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*MACRO*/
