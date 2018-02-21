cf5a1ab525f31b84861d573af291dec35329157b
bchen528 <bchen720@gmail.com>
2018-02-21 21:25:35 +0000

    add prototype

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef CHEESE
 #define CHEESE
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /*CHEESE*/
