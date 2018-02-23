5bead2232aa8c374f43b3a55b94cd8802cee4830
bchen528 <bchen720@gmail.com>
2018-02-23 19:37:05 +0000

    first commit

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - print strings
+ * @separator: string to be orinter between the strings
+ * @n: number of strings passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	va_start(valist, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (valist == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+				printf("%s", va_arg(valist, char *));
+				if (i != n - 1)
+					printf("%s", separator);
+			}
+		}
+		putchar('\n');
+		va_end(valist);
+	}
+}
