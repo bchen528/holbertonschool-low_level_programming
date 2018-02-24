b28fd83cf5249877f3d9dd9fe06db1fb94109a01
bchen528 <bchen720@gmail.com>
2018-02-24 21:13:34 +0000

    betty check

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -13,17 +13,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
-	
+
 	va_start(valist, n);
-	
+
 	if (separator == NULL)
 		separator = "";
-	
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
 		if (i != (n - 1))
-				printf("%s", separator);
+			printf("%s", separator);
 	}
 	putchar('\n');
 	va_end(valist);
