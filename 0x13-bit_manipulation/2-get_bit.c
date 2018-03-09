f05d54d29df30623662d7788a6187c7585a260c6
bchen528 <bchen720@gmail.com>
2018-03-09 03:32:44 +0000

    first commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_bit - finds value of bit at a given index
+ * @n: decimal number to be assessed
+ * @index: index starting from 0 of bit you want to get
+ * Return: value of bit at index or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bitval;
+
+	if (((n >> index) & 1) == 1 || ((n >> index) & 1) == 0)
+	{
+		bitval = n >> index & 1;
+		return (bitval);
+	}
+	return (-1);
+}
