8432aa4de17ab6f67388bd598876767fed4702be
bchen528 <bchen720@gmail.com>
2018-03-09 02:09:20 +0000

    first commit

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints binary representation of number
+ * @n: decimal number to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int mask;
+	int first_one = 0;
+
+/*create mask*/
+	mask = 0;
+	mask = ~mask;
+	mask = mask >> 1;
+	mask = ~(mask);
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			if (first_one == 1)
+				_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+			first_one = 1;
+		}
+		mask = mask >> 1;
+	}
+}
