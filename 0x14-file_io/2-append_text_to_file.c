84cae183548ce477015a1b4ce922ccfec8fb64c6
bchen528 <bchen720@gmail.com>
2018-03-13 23:00:41 +0000

    account for text_content NULL case

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -32,7 +32,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		text_conent = "";
 
 	len = _strlen(text_content);
 
