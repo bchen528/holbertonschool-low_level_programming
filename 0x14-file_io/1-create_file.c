3d9f4cea94aa97656a38bf5cdeab0cde6523c500
bchen528 <bchen720@gmail.com>
2018-03-13 21:40:30 +0000

    account for text_content NULL EC

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -32,11 +32,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
-		close(fd);
-		return (1);
-	}
+		text_content = "";
 
 /*create a file*/
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
