71e53937430949b332e78701044de6a6f18e9403
bchen528 <bchen720@gmail.com>
2018-03-13 21:45:14 +0000

    change position of len initialization

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -26,13 +26,15 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int len = _strlen(text_content);
+	int len;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
+
+	len = _strlen(text_content);
 
 /*create a file*/
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
