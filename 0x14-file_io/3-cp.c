31261c23bd919ecefde793caacc4e25be8db2f40
bchen528 <bchen720@gmail.com>
2018-03-14 03:11:33 +0000

    change permission back

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	}
 
 /*create and/or open file_to*/
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fd2 == -1)
 	{
