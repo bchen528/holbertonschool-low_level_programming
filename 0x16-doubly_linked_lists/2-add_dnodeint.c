103f09451b8bd7e3df4d133ee90237fdb38b20f5
bchen528 <bchen720@gmail.com>
2018-04-13 18:15:49 +0000

    consider null head

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -12,17 +12,22 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
+	if (*head == NULL)
+	{
+		new->n = n;
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	(*head)->prev = new;
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
-
 	*head = new;
 	return (new);
 }
