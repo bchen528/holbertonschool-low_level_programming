10f770921fe9161bfee7b74bcb77fb88d70bcc59
bchen528 <bchen720@gmail.com>
2018-04-15 22:36:55 +0000

    add null case

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *previous = NULL;
 	unsigned int count = 0;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL && head == NULL)
 		return (-1);
 
 	current = *head;
