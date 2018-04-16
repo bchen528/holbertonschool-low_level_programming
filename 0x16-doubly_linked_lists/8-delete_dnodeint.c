7064cd9c02591b2e78e03a7c48d6d3be466ef358
bchen528 <bchen720@gmail.com>
2018-04-16 00:23:49 +0000

    delete extra conditions

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -72,10 +72,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (current->next == NULL)
 		return (delete_end(current));
 /*delete middle node*/
-	if (current->prev != NULL)
-		previous->next = current->next;
-	if(current->next != NULL)
-		(current->next)->prev = previous;
+	previous->next = current->next;
+	(current->next)->prev = previous;
 	free(current);
 	return (1);
 }
