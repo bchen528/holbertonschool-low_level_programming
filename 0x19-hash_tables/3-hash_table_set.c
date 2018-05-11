014f84b6d3ee86e09ed831a669d0b1fefe0133e3
bchen528 <bchen720@gmail.com>
2018-05-11 00:20:50 +0000

    strdup

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -22,8 +22,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new == NULL)
 		return (0);
 
-	new->key = (char *)key;
-	new->value = (char *)value;
+	new->key = strdup(key);
+	new->value = strdup(value);
 	new->next = NULL;
 
 /*collision*/
