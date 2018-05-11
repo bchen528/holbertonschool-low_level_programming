a013d38795a36b8ceff7dfc9c0f3e0844acf3bb8
bchen528 <bchen720@gmail.com>
2018-05-11 17:59:17 +0000

    remove strdup

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -15,12 +15,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	const unsigned char *tmp_key = (const unsigned char *)(key);
 	int index = key_index(tmp_key, ht->size);
 
+	if (value == 0)
+		return (0);
+
 	new = malloc(sizeof(hash_node_t *));
 	if (new == NULL)
 		return (0);
 
-	new->key = strdup(key);
-	new->value = strdup(value);
+	new->key = (char *)key;
+	new->value = (char *)value;
 	new->next = NULL;
 
 /*collision*/
