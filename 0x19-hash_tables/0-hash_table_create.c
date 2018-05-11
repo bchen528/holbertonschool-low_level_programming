3e3162ab1a33ef0919ae2e83bc6ffaa272ed9f8e
bchen528 <bchen720@gmail.com>
2018-05-11 00:19:54 +0000

    change malloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,24 +12,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hashTable = NULL;
 	unsigned long int i = 0;
 
-	if (size > 0)
+	hashTable = malloc(sizeof(hash_table_t));
+	if (hashTable == NULL)
+		return (NULL);
+
+	hashTable->size = size;
+	hashTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashTable->array == NULL)
 	{
-		hashTable = malloc(sizeof(hash_table_t) * size);
-		if (hashTable == NULL)
-			return (NULL);
-		hashTable->size = size;
-		hashTable->array = malloc(sizeof(hash_node_t) * size);
-		if (hashTable->array == NULL)
-		{
-			free(hashTable);
-			return (NULL);
-		}
-		while (i < size)
-		{
-			hashTable->array[i] = NULL;
-			i++;
-		}
-		return (hashTable);
+		free(hashTable);
+		return (NULL);
 	}
-	return (NULL);
+
+	while (i < size)
+	{
+		hashTable->array[i] = NULL;
+		i++;
+	}
+	return (hashTable);
 }
