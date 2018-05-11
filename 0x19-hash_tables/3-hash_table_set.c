8b396e8e8494762d10e2eb7ff18ec66e5641050d
bchen528 <bchen720@gmail.com>
2018-05-11 22:27:02 +0000

    remove while loop

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -22,20 +22,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 
 /*update value for existing key*/
-	while (i < ht->size)
+	temp = ht->array[i];
+	while (temp != NULL)
 	{
-		temp = ht->array[i];
-		while (temp != NULL)
+		if (strcmp(temp->key, key) == 0)
 		{
-			if (strcmp(temp->key, key) == 0)
-			{
-				free(temp->value);
-				temp->value = strdup(value);
-				return (1);
-			}
-			temp = temp->next;
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
 		}
-		i++;
+		temp = temp->next;
 	}
 
 /*collision*/
