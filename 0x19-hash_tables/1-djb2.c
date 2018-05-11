122c9445a2df972389bb2d0b681b23823ebc639b
bchen528 <bchen720@gmail.com>
2018-05-11 17:04:26 +0000

    replace comments

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - a really good hash function
+ *
+ * @str: str to be hashed
+ * Return: hash value
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
