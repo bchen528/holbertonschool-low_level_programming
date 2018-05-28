6406565d025985c8529019d73c237a2e68b11e68
bchen528 <bchen720@gmail.com>
2018-05-28 20:03:53 +0000

    first commit

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,66 @@
+#include "sort.h"
+
+/**
+ * swap - swap two elements
+ *
+ * @array: array
+ * @a: index of first element
+ * @b: index of second element
+ */
+
+void swap(int *array, int a, int b)
+{
+	int temp;
+
+	temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+}
+
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using
+ * the Shell sort algorithm
+ *
+ * @array: array
+ * @size: size of array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	int gap = 1;
+	int i = 0, j = 0;
+	size_t end;
+
+	if (array == NULL || size == 0)
+		return;
+
+	gap = gap * 3 + 1;
+
+	while (gap >= 1)
+	{
+		end = i + gap;
+		while (end < size)
+		{
+			if (array[i] > array[i + gap])
+			{
+				swap(array, i, i + gap);
+				j = i;
+				while (j - gap >= 0)
+				{
+					if (array[j - gap] > array[j])
+						swap(array, j - gap, j);
+					else
+						break;
+					j--;
+				}
+			}
+			i++;
+			end = i + gap;
+		}
+		i = 0;
+		gap = gap / 2;
+		if (gap != 1)
+			print_array(array, size);
+	}
+}
