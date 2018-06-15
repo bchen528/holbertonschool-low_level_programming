afc05f700b2100bc56d822a7cd5ebbdac2c75cdf
bchen528 <bchen720@gmail.com>
2018-06-15 23:20:12 +0000

    add documentation and edge case

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
+"""calculate island perimeter"""
+
 def island_perimeter(grid):
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if j - 1 >= 0 and grid[i][j - 1] != 1:
-                    perimeter += 1
-                if j + 1 <= len(grid[i]) - 1 and grid[i][j + 1] != 1:
-                    perimeter += 1
-                if i - 1 >= 0 and grid[i - 1][j] != 1:
-                    perimeter += 1
-                if i + 1 <= len(grid) - 1 and grid[i + 1][j] != 1:
-                    perimeter += 1
-    return perimeter
+    """find perimeter of island
+    Args:
+        grid (list of lists): contains 1 representing land and 0 representing water
+    Returns:
+        perimeter of land
+    """
+    if grid is not None and grid != [[]]:
+        perimeter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    if j - 1 >= 0 and grid[i][j - 1] != 1:
+                        perimeter += 1
+                    if j + 1 <= len(grid[i]) - 1 and grid[i][j + 1] != 1:
+                        perimeter += 1
+                    if i - 1 >= 0 and grid[i - 1][j] != 1:
+                        perimeter += 1
+                    if i + 1 <= len(grid) - 1 and grid[i + 1][j] != 1:
+                        perimeter += 1
+        return perimeter
