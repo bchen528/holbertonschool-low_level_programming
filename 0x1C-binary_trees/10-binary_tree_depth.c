cea869929e59d6923f51af0be3b2dbe653643e22
bchen528 <bchen720@gmail.com>
2018-07-05 18:20:53 +0000

    add null case

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -10,11 +10,13 @@ size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t depth = 0;
 
-	if (node->parent)
-		while (node->parent)
-		{
-			depth += 1;
-			node = node->parent;
-		}
+	if (!node)
+		return (0);
+
+	while (node->parent)
+	{
+		depth += 1;
+		node = node->parent;
+	}
 	return (depth);
 }
