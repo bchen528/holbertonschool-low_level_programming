3ab28214a624bcde950b23f8d6bf0b21012c512d
bchen528 <bchen720@gmail.com>
2018-07-05 18:42:18 +0000

    fix type

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -11,11 +11,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (0);
+
 	if ((tree->left || tree->right) && tree->parent)
 		return (1);
 
-	if (!tree->left && !tree->left)
-		return (0);
 	return (binary_tree_nodes(tree->left) +
 		binary_tree_nodes(tree->right) + 1);
 }
