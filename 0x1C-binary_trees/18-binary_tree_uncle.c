ec2280c521f08d541c1f3e952a8dd7c3bef2b90b
bchen528 <bchen720@gmail.com>
2018-07-05 18:36:41 +0000

    change node assignment

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -9,10 +9,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-	if (node->parent->left == node)
-		return (node->parent->parent->left);
-	else if (node->parent->right == node)
+	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
+	else if (node->parent == node->parent->parent->right)
+		return (node->parent->parent->left);
 	else
 		return (NULL);
 }
