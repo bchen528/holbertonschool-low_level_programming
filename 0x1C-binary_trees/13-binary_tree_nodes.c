2e8d66d3bcf69548e5f7f25d3fbc710318614247
bchen528 <bchen720@gmail.com>
2018-07-05 20:56:08 +0000

    remove condition

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -14,9 +14,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (0);
 
-	if ((tree->left || tree->right) && tree->parent)
-		return (1);
-
 	return (binary_tree_nodes(tree->left) +
 		binary_tree_nodes(tree->right) + 1);
 }
