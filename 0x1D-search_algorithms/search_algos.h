dbb2d2a65951813f08eca3342e5f85c18e1a783c
bchen528 <bchen720@gmail.com>
2018-08-07 23:11:13 +0000

    add prototypes for exponential search

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -11,4 +11,7 @@ int jump_search(int *array, size_t size, int value);
 int jump_linear_search(int *array, size_t start, size_t size,
 		       size_t jump_size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int exp_binary_search(int *array, size_t start, size_t end, int value);
+int find_min(int a, int b);
 #endif /*__SEARCH__*/
