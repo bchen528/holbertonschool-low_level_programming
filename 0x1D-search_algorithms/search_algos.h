506a5c8f3acfac145df6bcf9fae8c4c91d19868b
bchen528 <bchen720@gmail.com>
2018-08-07 21:01:54 +0000

    add prototypes

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -2,9 +2,12 @@
 #define __SEARCH__
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t start, size_t end);
-
+int jump_search(int *array, size_t size, int value);
+int jump_linear_search(int *array, size_t start, size_t size,
+		       size_t jump_size, int value);
 #endif /*__SEARCH__*/
