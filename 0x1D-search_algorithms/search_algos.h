20794b51da71ea30e0f27860ea663911cf144d64
bchen528 <bchen720@gmail.com>
2018-08-07 21:58:13 +0000

    add interpolation header

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -10,4 +10,5 @@ void print_array(const int *array, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 int jump_linear_search(int *array, size_t start, size_t size,
 		       size_t jump_size, int value);
+int interpolation_search(int *array, size_t size, int value);
 #endif /*__SEARCH__*/
