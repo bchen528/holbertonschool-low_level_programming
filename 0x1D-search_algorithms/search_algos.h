31875922cff5795b62cc3cb1a61df9bfddd2f3d3
bchen528 <bchen720@gmail.com>
2018-08-10 03:29:22 +0000

    add skiplist prototype

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -21,6 +21,25 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t start, size_t end);
@@ -36,4 +55,6 @@ int adv_bin_helper(int *array, size_t start, size_t end, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *jumplist_linear_search(listint_t *list, size_t start, size_t size,
 				  size_t jump_size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+size_t list_len(skiplist_t *h);
 #endif /*__SEARCH__*/
